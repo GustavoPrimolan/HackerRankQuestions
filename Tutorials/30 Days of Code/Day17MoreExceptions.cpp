24874f784ef7b1be33f0a509249b9906d5b9c543
Gustavo Primolan <gustavo.primolan@hotmail.com>
2018-01-28 15:28:43 -0200

    Atualização

---
+++ Tutorials/30 Days of Code/Day17MoreExceptions.cpp
@@ -0,0 +1,45 @@
+#include <cmath>
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+#include <stdio.h>
+
+
+using namespace std;
+
+//Write your code here
+
+class Calculator{
+    public:
+        int power(int n, int p){
+            if(n == 0 )
+                return 0;
+            int pow = 1;
+            if(n < 0 || p < 0){
+                throw invalid_argument("n and p should be non-negative");
+            }
+            for(int i = 0; i < p; i++)
+                pow = pow * n;
+            
+            return pow;
+        }
+};
+
+int main()
+{
+    Calculator myCalculator=Calculator();
+    int T,n,p;
+    cin>>T;
+    while(T-->0){
+      if(scanf("%d %d",&n,&p)==2){
+         try{
+               int ans=myCalculator.power(n,p);
+               cout<<ans<<endl; 
+         }
+         catch(exception& e){
+             cout<<e.what()<<endl;
+         }
+      }
+    }
+    
+}
